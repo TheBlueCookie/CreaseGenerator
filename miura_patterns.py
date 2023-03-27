b03bcbc726acd2d734c038574cac61ccf9b00d4a
Elias Ankerhold <elias.ankerhold@gmail.com>
2023-03-27 21:14:39 +0300

    sweeped fold angle

--- miura_patterns.py
+++ miura_patterns.py
@@ -4,25 +4,26 @@ import numpy as np
 from patterns_lib import MiuraOriFold
 
 # paper size in mm
-din_a4 = np.array([210, 297])
-din_a3 = np.array([297, 420])
+din_a4_portrait = np.array([210, 297])
+din_a3_portrait = np.array([297, 420])
 
-square = np.array([1000, 1000])
+paper_size = din_a4_portrait
+cells_per_row = 5
+cells_per_column = 4
 
-paper_size = din_a4
-cells_per_row = 12
-cells_per_column = 8
+fold_angles = np.linspace(45, 85, 10)
 
-# does not yet correspond to proper angle, 90deg = rectangular pattern
-fold_angle = 60
+for ang in fold_angles:
+    fold_angle = ang
 
-miura_pattern = MiuraOriFold(paper_size=paper_size, n_cells_x=cells_per_row, n_cells_y=cells_per_column,
-                             fold_angle=fold_angle / 180.0 * np.pi, fit_end_vertex=True)
+    miura_pattern = MiuraOriFold(paper_size=paper_size, n_cells_x=cells_per_row, n_cells_y=cells_per_column,
+                                 fold_angle=fold_angle / 180.0 * np.pi, fit_end_vertex=True)
 
-miura_pattern.generate_pattern_from_unit_cell()
+    miura_pattern.generate_pattern_from_unit_cell()
 
-fig, ax = miura_pattern.plot_pattern()
+    # fig, ax = miura_pattern.plot_pattern()
+    # plt.show()
 
-miura_pattern.export(format='pdf')
+    # miura_pattern.export(format='pdf')
 
-plt.show()
+
